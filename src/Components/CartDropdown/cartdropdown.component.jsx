284c87b52ad2d54ebdd35ed29ad51bda4cb9a4b7
Jason Wan <jaysun1w@gmail.com>
2022-05-24 15:15:11 -0500

    cartdropdown visual func added

--- src/Components/CartDropdown/cartdropdown.component.jsx
+++ src/Components/CartDropdown/cartdropdown.component.jsx
@@ -1,11 +1,20 @@
 import Button from "../Button/button.component";
 import "../CartDropdown/cartdropdown.styles.scss";
+import CartItem from "../CartItem/cartitem.component";
+import { useContext } from "react";
+import { CartContext } from "../../contexts/cart.context";
 
 const CartDropdown = () =>{
 
+    const {cartItems} = useContext(CartContext);
+
     return(
         <div className="cart-dropdown-container">
-            <div className="cart-items"/>
+            <div className="cart-items">
+                {cartItems.map((item)=>
+                    <CartItem key={item.id} cartItem={item}></CartItem>
+                )}
+            </div>
             <Button>Checkout</Button>
         </div>
     );
